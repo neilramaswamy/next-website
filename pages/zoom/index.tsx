728a9367adec9cc09d0c20f67f0279104bcea90f
Neil Ramaswamy <neil@ramaswamy.org>
2021-02-09 10:24:30 -0500

    Try again: use getInitialProps correctly

--- pages/zoom/index.tsx
+++ pages/zoom/index.tsx
@@ -1,15 +1,30 @@
+import { NextPageContext, NextPage } from 'next'
+import { InternalLinks } from 'config/config'
+import { useRouter } from 'next/router'
 import React from 'react'
 
-const Zoom = (): JSX.Element => {
-    const zoomLink = process.env['ZOOM_LINK']
-
-    React.useEffect(() => {
-        if (zoomLink) {
-            window.location.href = zoomLink
-        }
-    }, [])
-
-    return zoomLink ? <p>You will be redirected shortly.</p> : <p>Oh no! No Zooming today :(</p>
+interface Props {
+    Location: string
 }
 
+const Zoom: NextPage<unknown> = (props: Props): JSX.Element => {
+    const { Location } = props
+    const router = useRouter()
+
+    React.useEffect(() => {
+        router.push(Location)
+    }, [])
+
+    console.log(Location)
+    return <p>You should be redirected shortly.</p>
+}
+
+Zoom.getInitialProps = (): Props => {
+    const zoomLink = process.env['ZOOM_LINK']
+    if (zoomLink) {
+        return { Location: zoomLink }
+    }
+
+    return { Location: InternalLinks.Home }
+}
 export default Zoom
